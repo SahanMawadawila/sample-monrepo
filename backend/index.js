f94e5f71236233d3ddd82cc7c9ea5d5a7ef67d41
SahanMawadawila <sahansithira12@gmail.com>
2026-02-17 18:25:57 +0530

    add liveness probe checker to backend

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => res.send("OK"));
 // Route 1: Home
 app.get("/api", (req, res) => {
   res.json({ message: "Welcome to the API" });
